0988512dff9c39597ec2541d035cbd3d7d252fa0
wu.shengbo <wu.shb@reachauto.com>
2020-06-01 15:36:14 +0800

    fix # Optimize code. Signed-off-by: wucn781290982@gmail.com

--- app/src/main/java/com/warchaser/glsurfaceviewdev/app/AppManager.java
+++ app/src/main/java/com/warchaser/glsurfaceviewdev/app/AppManager.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.hardware.camera2.CameraManager;
 
+import java.util.Iterator;
 import java.util.Stack;
 
 /**
@@ -43,7 +44,15 @@ public class AppManager {
      */
     public void removeActivity(Activity activity) {
         if (mActivityStack != null && !mActivityStack.isEmpty()) {
-            mActivityStack.remove(activity);
+            Iterator<Activity> iterator = mActivityStack.iterator();
+            while (iterator.hasNext()) {
+                Activity tmps = iterator.next();
+                if (tmps.equals(activity)) {
+                    iterator.remove();
+                    break;
+                }
+            }
+//            mActivityStack.remove(activity);
         }
     }
 
@@ -73,14 +82,15 @@ public class AppManager {
 
     /**
      * 将Stack中的Activity强转为Class<?>的实例
+     *
      * @param cls 继承自Activity的Class
      * @return 能转换的情况下，转换为Class<?>
-     * */
-    public <T extends Activity> T getLastActivity(Class<T> cls){
+     */
+    public <T extends Activity> T getLastActivity(Class<T> cls) {
 
         Activity activity = getLastActivity();
 
-        if(cls.isInstance(activity)){
+        if (cls.isInstance(activity)) {
             return cls.cast(activity);
         }
 
@@ -110,9 +120,17 @@ public class AppManager {
      * 结束指定类名的Activity
      */
     public void finishActivity(Class<?> cls) {
-        for (Activity activity : mActivityStack) {
-            if (activity.getClass().equals(cls)) {
-                finishActivity(activity);
+
+        if (mActivityStack == null || mActivityStack.isEmpty()) {
+            return;
+        }
+
+        Iterator<Activity> iterator = mActivityStack.iterator();
+        while (iterator.hasNext()) {
+            Activity tmps = iterator.next();
+            if (cls.equals(tmps.getClass())) {
+                iterator.remove();
+                tmps.finish();
                 break;
             }
         }
@@ -122,12 +140,23 @@ public class AppManager {
      * 结束除当前传入以外所有Activity
      */
     public void finishOthersActivity(Class<?> cls) {
-        if (mActivityStack != null)
-            for (Activity activity : mActivityStack) {
-                if (!activity.getClass().equals(cls)) {
-                    activity.finish();
+        if (mActivityStack != null) {
+
+            Iterator<Activity> iterator = mActivityStack.iterator();
+            while (iterator.hasNext()) {
+                Activity tmps = iterator.next();
+                if (!cls.equals(tmps.getClass())) {
+                    iterator.remove();
+                    tmps.finish();
                 }
             }
+        }
+
+//            for (Activity activity : mActivityStack) {
+//                if (!activity.getClass().equals(cls)) {
+//                    activity.finish();
+//                }
+//            }
     }
 
     /**
@@ -135,11 +164,15 @@ public class AppManager {
      */
     public void finishAllActivity() {
 
-        if (mActivityStack != null) {
-            for (Activity activity : mActivityStack) {
-                activity.finish();
-            }
-            mActivityStack.clear();
+        if (mActivityStack == null || mActivityStack.isEmpty()) {
+            return;
+        }
+
+        Iterator<Activity> iterator = mActivityStack.iterator();
+        while (iterator.hasNext()) {
+            Activity tmps = iterator.next();
+            iterator.remove();
+            tmps.finish();
         }
     }
 
@@ -156,7 +189,7 @@ public class AppManager {
         }
     }
 
-    public CameraManager getCameraManager(Activity activity){
+    public CameraManager getCameraManager(Activity activity) {
         return (CameraManager) activity.getSystemService(Context.CAMERA_SERVICE);
     }
 }
